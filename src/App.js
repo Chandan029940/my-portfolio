04327c74c95b25b370cb09697d729c8a05a29833
Chandan Patel <patelchandan2994@gmail.com>
2026-01-04 19:59:39 +0530

    Update portfolio

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Services from "./Components/Services/Services";
 import Experience from "./Components/Experience/Experience";
 import Works from "./Components/Works/Works";
 import Portfolio from "./Components/Portfolio/Portfolio";
-import Testimonals from "./Components/Testimonials/Testimonials";
+// import Testimonals from "./Components/Testimonials/Testimonials";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 import { themeContext } from "./Context";
